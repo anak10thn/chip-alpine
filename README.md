82f51663d9a9b7fbc43df57c91ed9a0ceb9445e0
anak10thn <anak10thn@gmail.com>
2018-03-10 19:58:28 +0700

    FIX some bugs

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 Alpine Linux for C.H.I.P.
 =========================
 
-Nightly builds of Alpine Linux for the C.H.I.P. computer.
+Nightly builds of Penyu Linux for the C.H.I.P. computer.
 Thanks to [Akidan](https://bbs.nextthing.co/users/Akidan) for [the initial research/HOWTO](https://bbs.nextthing.co/t/c-h-i-p-now-runs-alpine-linux/5109).
 
 ## Installation
 
-* Download [the latest release](https://github.com/marvinroger/chip-alpine/releases).
-* Extract it: `tar zxvf alpine*.tar.gz`
-* From within the C.H.I.P. SDK: `sudo BUILDROOT_OUTPUT_DIR=alpine/ ./chip-fel-flash.sh`
+* Download [the latest release](https://github.com/anak10thn/chip-penyu/releases).
+* Extract it: `tar zxvf penyu*.tar.gz`
+* From within the C.H.I.P. SDK: `sudo BUILDROOT_OUTPUT_DIR=penyu/ ./chip-fel-flash.sh`
 
 ## Usage
 
--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 set -eo pipefail
 
-readonly ALPINE_CHROOT_INSTALL_VERSION="0.6.0"
-readonly ALPINE_VERSION="latest-stable"
+readonly PENYU_CHROOT_INSTALL_VERSION="0.6.0"
+readonly PENYU_VERSION="latest-stable"
 readonly LATEST_BUILDROOT_URL="http://opensource.nextthing.co/chip/buildroot/stable/latest"
 
-readonly GITHUB_REPO="marvinroger/chip-alpine"
-readonly GITHUB_LOGIN_USERNAME="marvinroger"
+readonly GITHUB_REPO="orcinustools/penyu"
+readonly GITHUB_LOGIN_USERNAME="anak10thn"
 # secure readonly GITHUB_ACCESS_TOKEN
 
 die () {
@@ -24,7 +24,22 @@ ewarn () {
 }
 
 install_apt_dependencies () {
-  apt-get install -y git liblzo2-dev python-lzo mtd-utils
+cp /etc/apt/sources.list /etc/apt/sources.list.ori
+cat << EOF > /etc/apt/sources.list
+deb http://kebo.pens.ac.id/ubuntu/ zesty main restricted
+deb http://kebo.pens.ac.id/ubuntu/ zesty-updates main restricted
+deb http://kebo.pens.ac.id/ubuntu/ zesty universe
+deb http://kebo.pens.ac.id/ubuntu/ zesty-updates universe
+deb http://kebo.pens.ac.id/ubuntu/ zesty multiverse
+deb http://kebo.pens.ac.id/ubuntu/ zesty-updates multiverse
+deb http://kebo.pens.ac.id/ubuntu/ zesty-backports main restricted universe multiverse
+deb http://kebo.pens.ac.id/ubuntu/ zesty-security main restricted
+deb http://kebo.pens.ac.id/ubuntu/ zesty-security universe
+deb http://kebo.pens.ac.id/ubuntu/ zesty-security multiverse
+EOF
+
+apt-get update
+apt-get install -y git liblzo2-dev python-lzo mtd-utils python-setuptools wget
 }
 
 install_ubi_reader () {
@@ -37,29 +52,29 @@ install_ubi_reader () {
   rm --recursive "${temp_dir}"
 }
 
-install_alpine_chroot_install () {
+install_penyu_chroot_install () {
   local version="${1}"
 
-  wget --quiet --output-document /usr/local/bin/alpine-chroot-install "https://raw.githubusercontent.com/alpinelinux/alpine-chroot-install/v${version}/alpine-chroot-install"
-  chmod +x /usr/local/bin/alpine-chroot-install
+  wget --quiet --output-document /usr/local/bin/penyu-chroot-install "https://raw.githubusercontent.com/alpinelinux/alpine-chroot-install/v${version}/alpine-chroot-install"
+  chmod +x /usr/local/bin/penyu-chroot-install
 }
 
 get_latest_buildroot () {
   local latest_buildroot_url="${1}"
   local buildroot_dir="${2}"
-  
+
   local _latest_buildroot # _ because else conflict with latest_buildroot in main scope
   _latest_buildroot=$(wget --quiet -O- "${latest_buildroot_url}")
   eval "${3}=\"${_latest_buildroot}\""
   local buildroot_rootfs_url
   buildroot_rootfs_url="${_latest_buildroot}/images/rootfs.ubi"
-  
+
   local temp_dir
   temp_dir=$(mktemp -d -p /tmp buildroot.XXXXXX)
-  
+
   # download buildroot
   wget --quiet --output-document "${temp_dir}/rootfs.ubi" "${buildroot_rootfs_url}"
-  
+
   # extract ubi
   pushd "${temp_dir}"
   mkdir extracted
@@ -77,70 +92,70 @@ get_latest_buildroot () {
   rm --recursive "${temp_dir}"
 }
 
-prepare_alpine () {
-  local alpine_version="${1}"
-  local alpine_dir="${2}"
+prepare_penyu () {
+  local penyu_version="${1}"
+  local penyu_dir="${2}"
 
-  CHROOT_KEEP_VARS="" ALPINE_PACKAGES="wpa_supplicant wireless-tools bkeymaps tzdata nano" alpine-chroot-install -d "${alpine_dir}" -a armhf -b "${alpine_version}"
-  
-  "${alpine_dir}/enter-chroot" -u root <<-EOF
+  CHROOT_KEEP_VARS="" ALPINE_PACKAGES="wpa_supplicant wireless-tools bkeymaps tzdata nano" penyu-chroot-install -d "${penyu_dir}" -a armhf -b "${penyu_version}"
+
+  "${penyu_dir}/enter-chroot" -u root <<-EOF
     set -e
     # Needed services
     rc-update add devfs sysinit
     rc-update add dmesg sysinit
     rc-update add mdev sysinit
-  
+
     rc-update add modules boot
     rc-update add sysctl boot
     rc-update add hostname boot
     rc-update add bootmisc boot
     rc-update add syslog boot
     rc-update add wpa_supplicant boot # needed, otherwise does not connect after reboot
-  
+
     rc-update add mount-ro shutdown
     rc-update add killprocs shutdown
     rc-update add savecache shutdown
-  
+
     # Allow root login with no password.
     passwd root -d
-  
+
     # Allow root login from serial.
     echo ttyS0 >> /etc/securetty
     echo ttyGS0 >> /etc/securetty
-  
+
     # Make sure the USB virtual serial device is available.
     echo g_serial >> /etc/modules
-  
+
     # Make sure wireless networking is available.
     echo 8723bs >> /etc/modules
-  
+
     # These enable the USB virtual serial device, and the standard serial
     # pins to both be used as TTYs
     echo ttyS0::respawn:/sbin/getty -L ttyS0 115200 vt102 >> /etc/inittab
     echo ttyGS0::respawn:/sbin/getty -L ttyGS0 115200 vt102 >> /etc/inittab
 EOF
-  
-  umount -l "${alpine_dir}/proc"
-  umount -l "${alpine_dir}/sys"
-  umount -l "${alpine_dir}/dev"
-  umount -l "${alpine_dir}$(pwd)"
-  
-  rm "${alpine_dir}/usr/bin/qemu-arm-static"
-  rm "${alpine_dir}/etc/resolv.conf"
-  rm "${alpine_dir}/enter-chroot"
+
+  umount -l "${penyu_dir}/proc"
+  umount -l "${penyu_dir}/sys"
+  umount -l "${penyu_dir}/dev"
+  umount -l "${penyu_dir}$(pwd)"
+
+  rm "${penyu_dir}/usr/bin/qemu-arm-static"
+  rm "${penyu_dir}/etc/resolv.conf"
+  rm "${penyu_dir}/enter-chroot"
 }
 
 prepare_rootfs () {
   local buildroot_dir="${1}"
-  local alpine_dir="${2}"
+  local penyu_dir="${2}"
   local ubi_dest="${3}"
-  
+
   local temp_dir
   temp_dir=$(mktemp -d -p /tmp ubi.XXXXXX)
-  
-  cp --archive "${buildroot_dir}/boot/." "${alpine_dir}/boot"
-  cp --archive "${buildroot_dir}/lib/modules/." "${alpine_dir}/lib/modules"
-  
+
+  cp --archive "${buildroot_dir}/boot/." "${penyu_dir}/boot"
+  cp --archive "${buildroot_dir}/lib/modules/." "${penyu_dir}/lib/modules"
+
   pushd "${temp_dir}"
   cat <<EOF >ubinize.cfg
   [ubifs]
@@ -152,39 +167,39 @@ prepare_rootfs () {
   vol_flags=autoresize
   image=rootfs.ubifs
 EOF
-  mkfs.ubifs -d "${alpine_dir}" -o rootfs.ubifs -e 0x1f8000 -c 2000 -m 0x4000 -x lzo
+  mkfs.ubifs -d "${penyu_dir}" -o rootfs.ubifs -e 0x1f8000 -c 2000 -m 0x4000 -x lzo
   ubinize -o "${ubi_dest}" -m 0x4000 -p 0x200000 -s 16384 ubinize.cfg
   popd
   rm --recursive "${temp_dir}"
 }
 
-make_alpine_release () {
+make_penyu_release () {
   local chip_build_dir="${1}"
   local latest_buildroot="${2}"
   local tar_dest="${3}"
-  
-  wget --quiet --output-document "${chip_build_dir}/alpine/images/sun5i-r8-chip.dtb" "${latest_buildroot}/images/sun5i-r8-chip.dtb"
-  wget --quiet --output-document "${chip_build_dir}/alpine/images/sunxi-spl.bin" "${latest_buildroot}/images/sunxi-spl.bin"
-  wget --quiet --output-document "${chip_build_dir}/alpine/images/sunxi-spl-with-ecc.bin" "${latest_buildroot}/images/sunxi-spl-with-ecc.bin"
-  wget --quiet --output-document "${chip_build_dir}/alpine/images/uboot-env.bin" "${latest_buildroot}/images/uboot-env.bin"
-  wget --quiet --output-document "${chip_build_dir}/alpine/images/zImage" "${latest_buildroot}/images/zImage"
-  wget --quiet --output-document "${chip_build_dir}/alpine/images/u-boot-dtb.bin" "${latest_buildroot}/images/u-boot-dtb.bin"
-  
+
+  wget --quiet --output-document "${chip_build_dir}/penyu/images/sun5i-r8-chip.dtb" "${latest_buildroot}/images/sun5i-r8-chip.dtb"
+  wget --quiet --output-document "${chip_build_dir}/penyu/images/sunxi-spl.bin" "${latest_buildroot}/images/sunxi-spl.bin"
+  wget --quiet --output-document "${chip_build_dir}/penyu/images/sunxi-spl-with-ecc.bin" "${latest_buildroot}/images/sunxi-spl-with-ecc.bin"
+  wget --quiet --output-document "${chip_build_dir}/penyu/images/uboot-env.bin" "${latest_buildroot}/images/uboot-env.bin"
+  wget --quiet --output-document "${chip_build_dir}/penyu/images/zImage" "${latest_buildroot}/images/zImage"
+  wget --quiet --output-document "${chip_build_dir}/penyu/images/u-boot-dtb.bin" "${latest_buildroot}/images/u-boot-dtb.bin"
+
   pushd "${chip_build_dir}"
-  tar -zcv -C "${chip_build_dir}" -f "${tar_dest}" alpine
+  tar -zcv -C "${chip_build_dir}" -f "${tar_dest}" penyu
   popd
 }
 
 gather_rootfs_versions () {
   local buildroot_dir="${1}"
-  local alpine_dir="${2}"
-  
+  local penyu_dir="${2}"
+
   # shellcheck source=/dev/null
   source "${buildroot_dir}/etc/os-release"
   eval "${3}=\"${VERSION_ID}\""
-  
+
   # shellcheck source=/dev/null
-  source "${alpine_dir}/etc/os-release"
+  source "${penyu_dir}/etc/os-release"
   eval "${4}=\"${VERSION_ID}\""
   eval "${5}=\"${PRETTY_NAME}\""
 }
@@ -197,87 +212,86 @@ release_github () {
   local release_name="${5}"
   local release_body="${6}"
   local tar_location="${7}"
-  
+
   local release_json
   release_json=$(printf '{"tag_name": "%s","target_commitish": "master","name": "%s","body": "%s","draft": false,"prerelease": false}' "${tag_name}" "${release_name}" "${release_body}")
   local github_release_id
   github_release_id=$(curl -u "${username}:${access_token}" --data "${release_json}" -v --silent "https://api.github.com/repos/${repo}/releases" 2>&1 | sed -ne 's/^  "id": \(.*\),$/\1/p')
-  
+
   curl -u "${username}:${access_token}" -X POST -H "Content-Type: application/gzip" --data-binary "@${tar_location}" "https://uploads.github.com/repos/${repo}/releases/${github_release_id}/assets?name=${tag_name}.tar.gz"
 }
 
 main () {
   local working_dir
-  working_dir=$(mktemp -d -p /tmp chip-alpine.XXXXXX)
+  working_dir=$(mktemp -d -p /tmp chip-penyu.XXXXXX)
   local buildroot_dir="${working_dir}/buildroot"
   mkdir -p "${buildroot_dir}"
-  local alpine_dir="${working_dir}/alpine"
-  mkdir -p "${alpine_dir}"
+  local penyu_dir="${working_dir}/penyu"
+  mkdir -p "${penyu_dir}"
   local chip_build_dir="${working_dir}/chip-build"
-  mkdir -p "${chip_build_dir}/alpine/images"
-  
+  mkdir -p "${chip_build_dir}/penyu/images"
+
   einfo "Installing dependencies..."
   install_apt_dependencies
-  
+
   einfo "Installing ubi_reader..."
   install_ubi_reader
-  
-  einfo "Installing alpine-chroot-install..."
-  install_alpine_chroot_install "${ALPINE_CHROOT_INSTALL_VERSION}"
-  
+
+  einfo "Installing penyu-chroot-install..."
+  install_penyu_chroot_install "${PENYU_CHROOT_INSTALL_VERSION}"
+
   #####
   # Get the latest base buildroot image
   #####
-  
+
   einfo "Getting latest buildroot..."
   local latest_buildroot=""
   get_latest_buildroot "${LATEST_BUILDROOT_URL}" "${buildroot_dir}" "latest_buildroot"
-  
+
   #####
-  # Get and set-up Alpine
+  # Get and set-up Penyu
   #####
-  
-  einfo "Getting and setting-up Alpine..."
-  prepare_alpine "${ALPINE_VERSION}" "${alpine_dir}"
-  
+
+  einfo "Getting and setting-up Penyu..."
+  prepare_penyu "${PENYU_VERSION}" "${penyu_dir}"
+
   #####
   # Prepare rootfs
   #####
-  
+
   einfo "Preparing rootfs..."
-  prepare_rootfs "${buildroot_dir}" "${alpine_dir}" "${chip_build_dir}/alpine/images/rootfs.ubi"
-  
+  prepare_rootfs "${buildroot_dir}" "${penyu_dir}" "${chip_build_dir}/penyu/images/rootfs.ubi"
+
   #####
-  # Make Alpine release
+  # Make Penyu release
   #####
-  
-  einfo "Making Alpine release..."
+
+  einfo "Making Penyu release..."
   local temp_tar
   temp_tar=$(mktemp -p /tmp tar.XXXXXX)
-  make_alpine_release "${chip_build_dir}" "${latest_buildroot}" "${temp_tar}"
-  
+  make_penyu_release "${chip_build_dir}" "${latest_buildroot}" "${temp_tar}"
+
   einfo "Gathering rootfs versions..."
   local buildroot_version_id=""
-  local alpine_version_id=""
-  local alpine_pretty_name=""
-  gather_rootfs_versions "${buildroot_dir}" "${alpine_dir}" "buildroot_version_id" "alpine_version_id" "alpine_pretty_name"
-  
+  local penyu_version_id=""
+  local penyu_pretty_name=""
+  gather_rootfs_versions "${buildroot_dir}" "${penyu_dir}" "buildroot_version_id" "penyu_version_id" "penyu_pretty_name"
+
   #####
   # Create GitHub release
   #####
-  
-  einfo "Releasing on GitHub..."
-  release_github "${GITHUB_REPO}" "${GITHUB_LOGIN_USERNAME}" "${GITHUB_ACCESS_TOKEN}" \
-    "alpine-${alpine_version_id}_buildroot-${buildroot_version_id}_$(date +%s)" \
-    "${alpine_pretty_name} with Buildroot ${buildroot_version_id} built on $(date +%Y-%m-%d)" \
-    "Daily build." \
-    "${temp_tar}"
-    
-  rm "${temp_tar}"
 
-  einfo "Done!"  
+  # einfo "Releasing on GitHub..."
+  # release_github "${GITHUB_REPO}" "${GITHUB_LOGIN_USERNAME}" "${GITHUB_ACCESS_TOKEN}" \
+  #   "penyu-${penyu_version_id}_buildroot-${buildroot_version_id}_$(date +%s)" \
+  #   "${penyu_pretty_name} with Buildroot ${buildroot_version_id} built on $(date +%Y-%m-%d)" \
+  #   "Daily build." \
+  #   "${temp_tar}"
+
+  cp "${temp_tar}" /root/penyu-${penyu_version_id}-chip-armv7.tar.gz
+
+  einfo "Done!"
 }
 
 main
-
-# tar zxvf alpine*.tar.gz && sudo BUILDROOT_OUTPUT_DIR=alpine/ ./chip-fel-flash.sh
+# tar zxvf penyu*.tar.gz && sudo BUILDROOT_OUTPUT_DIR=penyu/ ./chip-fel-flash.sh
